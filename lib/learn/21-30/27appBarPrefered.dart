0ca94be2def2d61825dc2cc43f0d78274aad1fd1
Adi Munawar <57015700+AdiMunawar31@users.noreply.github.com>
2021-06-11 03:26:15 +0700

    25-27

---
+++ lib/learn/21-30/27appBarPrefered.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100),
+          child: AppBar(
+            backgroundColor: Colors.amber[600],
+            flexibleSpace: Positioned(
+              bottom: 0.0,
+              right: 0.0,
+              child: Container(
+                margin: EdgeInsets.all(20),
+                child: Text("D2Y CODING",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ),
+          ),
+        )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Main(),
-    );
-  }
-}
-
-class Main extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
+        debugShowCheckedModeBanner: false,
+        home: DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              backgroundColor: Colors.black,
+              appBar: AppBar(
+                title: Text("Library Music"),
+                backgroundColor: Colors.lightBlue[900],
+                bottom: TabBar(tabs: [
+                  Tab(
+                    icon: Icon(Icons.music_note_outlined),
+                    text: "TRACKS",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.supervisor_account_sharp),
+                    text: "ARTISTS",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.library_music_outlined),
+                    text: "ALBUMS",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.queue_music),
+                    text: "GENRES",
+                  ),
+                ]),
+              ),
+              body: TabBarView(children: [
+                Center(
+                    child: Text(
+                  "Tracks Page",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold),
+                )),
+                Center(
+                    child: Text(
+                  "Artists Page",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold),
+                )),
+                Center(
+                    child: Text(
+                  "Albums Page",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold),
+                )),
+                Center(
+                    child: Text(
+                  "Genres Page",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold),
+                )),
+              ]),
+            )));
   }
 }
